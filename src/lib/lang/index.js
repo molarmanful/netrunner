a1da77527d769d49071d94f060802311165d9676
molarmanful <benjinpang@gmail.com>
2023-10-19 23:50:48 -0400

    wip

--- src/lib/lang/index.js
+++ src/lib/lang/index.js
@@ -4,23 +4,39 @@ export default class Env {
   constructor() {
     this.stack = writable([])
     this.code = writable([])
+    this.scope = writable({})
     this.err = writable('')
 
+    Env.patch()
+    this.types = {
+      Fn,
+      Cmd,
+    }
+
     this.cmds = {
-      '.': () => this.f((a, b) => +(a + '.' + b)),
       _: () => this.f(a => -a),
+      '.': () => this.f((a, b) => +(a + '.' + b)),
       '+': () => this.f((a, b) => a - -b),
-      '++': () => this.f((a, b) => '' + a + b),
       '-': () => this.f((a, b) => a - b),
       '*': () => this.f((a, b) => a * b),
       '/': () => this.f((a, b) => a / b),
       '%': () => this.f((a, b) => a % b),
       '**': () => this.f((a, b) => a ** b),
+      '++': () => this.f((a, b) => '' + a + b),
       dup: () => this.fs(a => [a, a]),
       pop: () => this.pop(),
       swap: () => this.fs((a, b) => [b, a]),
       rot: () => this.fs((a, b, c) => [b, c, a]),
       rot_: () => this.fs((a, b, c) => [c, a, b]),
+    }
+  }
+
+  static patch() {
+    String.prototype.show = function () {
+      return JSON.stringify(this)
+    }
+    Number.prototype.show = function () {
+      return '' + this
     }
   }
 
@@ -39,8 +55,20 @@ export default class Env {
     return env
   }
 
+  showCode() {
+    return 'CODE:\n\n' + this.scope$.map(x => x.show()).join`\n`
+  }
+
   showStack() {
-    return 'STACK:\n\n' + this.stack$.map(x => JSON.stringify(x)).join`\n`
+    return 'STACK:\n\n' + this.stack$.map(x => x.show()).join`\n`
+  }
+
+  showScope() {
+    return (
+      'SCOPE:\n\n' +
+      Object.entries(this.scope$).map(([k, v]) => k + ' := ' + v.show())
+        .join`\n`
+    )
   }
 
   f(f) {
@@ -53,6 +81,10 @@ export default class Env {
     let n = f.length
     this.checkLen(n)
     this.push(...f(...this.last(n)))
+  }
+
+  setVar(k, v) {
+    this.scope.update(xs => ({ ...xs, [k]: v }))
   }
 
   push(...a) {
@@ -84,6 +116,10 @@ export default class Env {
     return Env.getSub(this.stack)
   }
 
+  get scope$() {
+    return Env.getSub(this.scope)
+  }
+
   get code$() {
     return Env.getSub(this.code)
   }
@@ -100,5 +136,33 @@ export default class Env {
     let a
     this.stack.update(xs => ((a = xs.splice(-n, n)), xs))
     return a
+  }
+}
+
+class Fn {
+  constructor(f) {
+    this.x = f
+  }
+
+  toString() {
+    return this.x.map(x => x.toString()).join`\n`
+  }
+
+  show() {
+    return `{${this.x.map(x => x.show()).join` `}}`
+  }
+}
+
+class Cmd {
+  constructor(f) {
+    this.x = f
+  }
+
+  toString() {
+    return this.x
+  }
+
+  show() {
+    return this.x
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,20 @@
   import Env from '$lib/lang'
 
   let env = new Env()
-  let { stack, code, err } = env
+  let { stack, code, scope, err } = env
 
   let num = false
+  let get = false
+  let set = false
   let n = 0
+  let vname = ''
+  let reset = () => {
+    num = false
+    get = false
+    set = false
+    n = 0
+    vname = ''
+  }
 </script>
 
 <svelte:head>
@@ -17,17 +27,37 @@
   <div class="flex gap-4">
     <Btn
       on:click={() => {
-        if (!num) num = true
-        else {
-          env.step(() => {
-            env.push(n)
-          })
-          num = false
-        }
+        if (!num) {
+          reset()
+          num = true
+        } else reset()
       }}
     >
       NUM
     </Btn>
+
+    <Btn
+      on:click={() => {
+        if (!get) {
+          reset()
+          get = true
+        } else reset()
+      }}
+    >
+      $
+    </Btn>
+
+    <Btn
+      on:click={() => {
+        if (!set) {
+          reset()
+          set = true
+        } else reset()
+      }}
+    >
+      $=
+    </Btn>
+
     {#each Object.entries(env.cmds) as [k, f]}
       <Cmd {env} {f} {k} />
     {/each}
@@ -37,15 +67,58 @@
 
   {#if num}
     {n}
+    <br />
     <input max="27" min="0" type="range" bind:value={n} />
+    <Btn
+      on:click={() => {
+        env.step(() => {
+          env.push(n)
+        })
+      }}
+    >
+      PUSH
+    </Btn>
+  {/if}
+
+  {#if get}
+    <input autofocus type="text" bind:value={vname} />
+    <Btn
+      on:click={() => {
+        env.step(() => {
+          let v = env.scope$[vname]
+          if (!vname) throw new Error(`empty var`)
+          if (v == void 0) throw new Error(`undefined var ${vname}`)
+          env.push(v)
+        })
+        vname = ''
+      }}
+    >
+      GET
+    </Btn>
+  {/if}
+
+  {#if set}
+    <input autofocus type="text" bind:value={vname} />
+    <Btn
+      on:click={() => {
+        env.step(() => {
+          if (!vname) throw new Error(`empty var`)
+          env.setVar(vname, env.pop())
+        })
+        vname = ''
+      }}
+    >
+      SET
+    </Btn>
   {/if}
 
   <hr />
   <br />
   {#if $err}
-    ERR: {$err}
+    <span class="c-red">ERR: {$err}</span>
   {/if}
-  <pre>
-{($stack, env.showStack())}
-  </pre>
+  <div class="flex">
+    <pre class="flex-1">{($stack, env.showStack())}</pre>
+    <pre class="flex-1">{($scope, env.showScope())}</pre>
+  </div>
 </main>
