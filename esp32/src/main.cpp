fbbc587a77dc1b0e3aaf6161ac06b14121fa726a
molarmanful <benjinpang@gmail.com>
2024-02-14 13:23:27 -0500

    iffy

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,5 +1,21 @@
-#include <Arduino.h>
+#include "deps.h"
+
+RingBuf<uint8_t, 10> r;
+int a, b;
 
 void setup() { Serial.begin(9600); }
 
-void loop() { Serial.println(analogRead(15)); }
+void loop() {
+  r.pushOverwrite(analogRead(15) * 9 / 4095);
+
+  int sum = 0;
+  for (int i = 0; i < r.size(); i++) {
+    sum += r[i];
+  }
+  b = sum / r.size();
+
+  if (a != b) {
+    a = b;
+    Serial.println(b);
+  }
+}
