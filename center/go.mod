aa1f3ca3cd1513e26751c819488505a890aa4ce7
molarmanful <benjinpang@gmail.com>
2024-02-28 00:37:25 -0500

    basic interpreter

--- center/go.mod
+++ center/go.mod
@@ -3,6 +3,7 @@ module netrunner
 go 1.21.3
 
 require (
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 )
--- center/main.go
+++ center/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 
+	"github.com/emirpasic/gods/stacks/arraystack"
 	"github.com/tarm/serial"
 )
 
@@ -13,12 +16,56 @@ func main() {
 		log.Fatal(e)
 	}
 
+	cur := 0
+	stack := arraystack.New()
+
 	for {
 		buf := make([]byte, 128)
 		n, e := s.Read(buf)
+		fmt.Print("\033[H\033[2J")
 		if e != nil {
 			log.Fatal(e)
 		}
-		log.Printf("%q", buf[:n])
+
+		for _, x := range string(buf[:n]) {
+			switch x {
+			case '0':
+				cur++
+				cur %= 10
+			case '1':
+				stack.Push(cur)
+			case '2':
+				if b, ok := stack.Pop(); ok {
+					if a, ok := stack.Pop(); ok {
+						sa := strconv.Itoa(a.(int))
+						sb := strconv.Itoa(b.(int))
+						s, e := strconv.Atoi(sa + sb)
+						if e != nil {
+							log.Fatal(e)
+						} else {
+							stack.Push(s)
+						}
+					}
+				}
+			case '3':
+				if b, ok := stack.Pop(); ok {
+					if a, ok := stack.Pop(); ok {
+						stack.Push(a.(int) + b.(int))
+					}
+				}
+			case '4':
+				if a, ok := stack.Pop(); ok {
+					stack.Push(-a.(int))
+				}
+			default:
+			}
+		}
+
+		fmt.Println("CUR:", cur)
+		fmt.Println("STACK:")
+		it := stack.Iterator()
+		for it.Next() {
+			fmt.Println(it.Value())
+		}
 	}
 }
